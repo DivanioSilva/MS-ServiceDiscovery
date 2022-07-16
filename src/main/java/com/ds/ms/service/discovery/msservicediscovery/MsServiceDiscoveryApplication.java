75accca89d750002eb7dff562de326d8a25c64b4
Divanio Silva <dcsilva@mail.ru>
2022-07-17 00:01:28 +0200

    First commit

---
+++ src/main/java/com/ds/ms/service/discovery/msservicediscovery/MsServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.ds.ms.service.discovery.msservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MsServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsServiceDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ds/ms/service/discovery/msservicediscovery/MsServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.ms.service.discovery.msservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
